bd23f32aa1efd93e098421fcff4e61199f4affc1
francis-gomes-esq <figomesokoye@protonmail.com>
2024-03-15 21:43:22 +0000

    add contact and fix issues

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,1 +1,32 @@
+import React from 'react'
 
+const About = () => {
+	return (
+		<div className='flex flex-col mt-4 transition-all duration-300 ease-in-out hover:-translate-x-2 hover:underline'>
+			<h1 className='text-3xl font-bold mb-4'>About Me</h1>
+			<div className='rounded-lg shadow-md p-6'>
+				<p className='text-lg mb-4'>
+					Hi, I'm Francis! Passionate front end developer with a
+					strong foundation in JavaScript, React, HTML, and CSS. Eager
+					to contribute to dynamic projects and collaborate with teams
+					to solve complex problems. With a focus on continuous
+					learning and growth, I strive to leverage my skills to
+					deliver efficient, user-centric solutions. Explore my
+					portfolio to see my projects and how I can add value to your
+					team.
+				</p>
+
+				<p className='text-lg'>
+					If you'd like to get in touch via email, feel free to email
+					me at{' '}
+					<a href='mailto:figomesokoye@protonmail.com'>
+						figomesokoye@protonmail.com
+					</a>
+					.
+				</p>
+			</div>
+		</div>
+	)
+}
+
+export default About
--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,4 +1,41 @@
 const Contact = () => {
-	return <div>Contact</div>
+	return (
+		<div className='container flex flex-col mt-4 transition-all duration-300 ease-in-out hover:-translate-x-2'>
+			<h1 className='text-3xl font-bold mb-4'>Contact Me</h1>
+			<div className='rounded-lg shadow-md p-6'>
+				<p className='text-lg mb-4'>You can find me on:</p>
+				<ul className='list-disc pl-6 mb-4'>
+					<li className='contact-list'>
+						<a
+							href='https://github.com/francis-gomes-esq'
+							className='text-blue-500 hover:text-blue-700'
+						>
+							GitHub
+						</a>
+					</li>
+					<li className='contact-list'>
+						<a
+							href='https://bit.ly/linkedin-fgomes-okoye'
+							className='text-blue-500 hover:text-blue-700'
+						>
+							LinkedIn
+						</a>
+					</li>
+					<li className='contact-list'>
+						<a
+							href='https://twitter.com/account'
+							className='text-blue-500 hover:text-blue-700'
+						>
+							Twitter
+						</a>
+					</li>
+				</ul>
+				<p className='text-lg'>
+					Feel free to connect with me on any of these platforms!
+				</p>
+			</div>
+		</div>
+	)
 }
+
 export default Contact
